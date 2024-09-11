b9fce19d64bd6929aaf23467c6fcf7d2441206f8
Arun Kumar M <marunkumar0203@gmail.com>
2024-09-11 10:07:21 +0530

    Max Sum of Subarray - Kadane's Algo

--- Day3.java
+++ Day3.java
@@ -6,14 +6,36 @@ public class Day3 {
 
         Scanner sc = new Scanner(System.in);
 
-        // Longest increasing subsequence
-        int n = sc.nextInt(); // 10
-        int[] arr = new int[n]; // -7 1 3 2 4 10 9 8 11 15
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
-        longestIncreasingSubSequence(arr); // 7
+//        // Longest increasing subsequence
+//        int n = sc.nextInt(); // 10
+//        int[] arr = new int[n]; // -7 1 3 2 4 10 9 8 11 15
+//        for(int i = 0; i<n; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        longestIncreasingSubSequence(arr); // 7
 
+
+//        Maximum sum of subarray
+//        int n = sc.nextInt(); // 10
+//        int[] arr = new int[n]; // 50 -100 20 30 -100 50 60 100 -200 -10
+//        for(int i = 0; i<n; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        subarrayMaxSum(arr); // 210 // Kadane's Algorithm
+    }
+
+    public static void subarrayMaxSum(int[] arr){
+        int maxSum = arr[0];
+        int currSum = arr[0];
+        for(int idx = 0; idx<arr.length; idx++){
+            if(currSum<0){
+                currSum = arr[idx];
+            }else{
+                currSum += arr[idx];
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+        System.out.println(maxSum);
     }
 
     public static void longestIncreasingSubSequence(int[] arr){
